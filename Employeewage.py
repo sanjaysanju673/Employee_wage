c74134c3918cc22ea3958d7ca084144b3720cf6e
sanjaysanju673 <sanjuvadde1111@gmail.com>
2024-08-06 11:55:58 +0530

    [Sanjay] Add:program to  refactor the uc6 program to class and methods

--- Employeewage.py
+++ Employeewage.py
@@ -1,101 +1,91 @@
 '''
     @Author: v sanjay kumar
-    @Date: 2024-07-02 01:00:30
+    @Date: 2024-07-03 02:00:30
     @Last Modified by: v sanjay kumar
-    @Last Modified time: 2024-07-02 01:00:30
+    @Last Modified time: 2024-07-03 02:00:30
     @Title : Employee wage problem
 '''
 
 import random
-def check_attendance():
-    """
-    Check the attendance for an employee.
 
-    Returns:
-    - 'full time' if employee is present full time
-    - 'part time' if employee is present part time
-    - 'absent' if employee is absent
-    """
-    attendance = random.randint(0, 2)
-    if attendance == 1:
-        return 'full time'
-    elif attendance == 2:
-        return 'part time'
-    else:
-        return 'absent'
+class Employee:
+    def __init__(self, name):
+        self.name = name
+        self.wage_per_hour = 20
+        self.full_time_hours = 8
+        self.part_time_hours = 4
+        self.total_hours = 0
+        self.total_days = 0
+        self.monthly_wage = 0
 
-def daily_wage(name, attendance):
-    """
-    Calculate the daily wage for a full-time employee based on attendance.
+    def check_attendance(self):
+        """
+        Check the attendance for an employee.
 
-    Parameters:
-    - name: Name of the employee
-    - attendance: Attendance status of the employee
-
-    Returns:
-    - Daily wage of the employee
-    """
-    full_hours = 8 if attendance == 'full time' else 0
-    wage_per_hour = 20
-    wage = wage_per_hour * full_hours
-    print(f'{name} wage is {wage}')
-    return wage
-
-def part_time(name, attendance):
-    """
-    Calculate the daily wage for a part-time employee based on attendance.
-
-    Parameters:
-    - name: Name of the employee
-    - attendance: Attendance status of the employee
-
-    Returns:
-    - Part-time wage of the employee
-    """
-    part_hours = 4 if attendance == 'part time' else 0
-    wage_per_hour = 20
-    wage = wage_per_hour * part_hours
-    print(f'{name} wage is {wage}')
-    return wage
-
-def wage_per_month(name):
-    """
-    Calculate the total monthly wage for an employee.
-
-    Parameters:
-    - name: Name of the employee
-
-    Returns:
-    - Total monthly wage of the employee
-    """
-    total_month_wage = 0
-    for _ in range(20):
-        attendance = check_attendance()
-        if attendance == 'full time':
-            total_month_wage += daily_wage(name, attendance)
-        elif attendance == 'part time':
-            total_month_wage += part_time(name, attendance)
-    print(f"{name} employee monthly wage is {total_month_wage}")
-    return total_month_wage
-
-def wage_per_codition(name,attendence):
-    total_days =0
-    total_hours =0
-    monthly_wages =[]
-    monthly_wage =0
-    while total_hours<=100 and total_days <= 20:
-        if(attendence=='full time'):
-            total_hours +=8
-            wage =daily_wage(name,attendence)
-            monthly_wages.append(wage)
-            monthly_wage +=wage
-        elif(attendence =='part time'):
-            total_hours +=4
-            wage = part_time(name,attendence)
+        Returns:
+        - 'full time' if employee is present full time
+        - 'part time' if employee is present part time
+        - 'absent' if employee is absent
+        """
+        attendance = random.randint(0, 2)
+        if attendance == 1:
+            return 'full time'
+        elif attendance == 2:
+            return 'part time'
         else:
-            monthly_wage.append(0)
-    print('Employee monthly wage',monthly_wage)
-    return monthly_wages
+            return 'absent'
+
+    def calculate_daily_wage(self, attendance):
+        """
+        Calculate the daily wage for an employee based on attendance.
+
+        Parameters:
+        - attendance: Attendance status of the employee
+
+        Returns:
+        - Daily wage of the employee
+        """
+        if attendance == 'full time':
+            hours = self.full_time_hours
+        elif attendance == 'part time':
+            hours = self.part_time_hours
+        else:
+            hours = 0
+        wage = self.wage_per_hour * hours
+        print(f'{self.name} wage is {wage}')
+        return wage
+
+    def calculate_monthly_wage(self):
+        """
+        Calculate the total monthly wage for an employee.
+        """
+        for _ in range(20):
+            attendance = self.check_attendance()
+            daily_wage = self.calculate_daily_wage(attendance)
+            self.monthly_wage += daily_wage
+        print(f"{self.name} employee monthly wage is {self.monthly_wage}")
+        return self.monthly_wage
+
+    def wage_per_condition(self):
+        """
+        Calculate the total monthly wage for an employee based on conditions.
+        """
+        while self.total_hours <= 100 and self.total_days <= 20:
+            attendance = self.check_attendance()
+            if attendance == 'full time':
+                self.total_hours += self.full_time_hours
+                wage = self.calculate_daily_wage(attendance)
+            elif attendance == 'part time':
+                self.total_hours += self.part_time_hours
+                wage = self.calculate_daily_wage(attendance)
+            else:
+                wage = 0
+            self.monthly_wage += wage
+            self.total_days += 1
+        print('Employee monthly wage', self.monthly_wage)
+        return self.monthly_wage
+
+
 def main():
     """
     Display the menu and handle user choices.
@@ -107,28 +97,34 @@ def main():
             print("2. Calculate Full-Time Daily Wage")
             print("3. Calculate Part-Time Daily Wage")
             print("4. Calculate Monthly Wage")
-            print("5. Exit")
+            print("5. Calculate Wage Per Condition")
+            print("6. Exit")
             choice = int(input("Enter Your Choice: "))
 
             if choice == 1:
-                attendance = check_attendance()
+                employee = Employee(input("Enter the name of the employee: "))
+                attendance = employee.check_attendance()
                 print(f"Attendance: {attendance}")
 
             elif choice == 2:
-                name = input("Enter the name of the employee: ")
-                attendance = check_attendance()
-                daily_wage(name, attendance)
+                employee = Employee(input("Enter the name of the employee: "))
+                attendance = employee.check_attendance()
+                employee.calculate_daily_wage(attendance)
 
             elif choice == 3:
-                name = input("Enter the name of the employee: ")
-                attendance = check_attendance()
-                part_time(name, attendance)
+                employee = Employee(input("Enter the name of the employee: "))
+                attendance = employee.check_attendance()
+                employee.calculate_daily_wage(attendance)
 
             elif choice == 4:
-                name = input("Enter the name of the employee: ")
-                wage_per_month(name)
+                employee = Employee(input("Enter the name of the employee: "))
+                employee.calculate_monthly_wage()
 
             elif choice == 5:
+                employee = Employee(input("Enter the name of the employee: "))
+                employee.wage_per_condition()
+
+            elif choice == 6:
                 print("Exiting the program.")
                 break
 
