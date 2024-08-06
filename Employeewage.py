40432accb966024489264eedd5030aa2da32c368
sanjaysanju673 <sanjuvadde1111@gmail.com>
2024-08-06 11:23:01 +0530

    [Sanjay] Add:program to find the employee daily wage

--- Employeewage.py
+++ Employeewage.py
@@ -1,10 +1,10 @@
 '''
     @Author: v sanjay kumar
-    @Date: 2024-07-30 11:00:30
+    @Date: 2024-07-30 01:00:30
     @Last Modified by: v sanjay kumar
-    @Last Modified time: 2024-07-30 11:00:30
+    @Last Modified time: 2024-07-30 01:00:30
     @Title : Employee wage problem
-  '''
+'''
 import random
 def check_attendance(self):
         """
@@ -22,9 +22,19 @@ def check_attendance(self):
         else:
             return 'absent'
 
+def daily_wage():
+    
+    if (check_attendance()=='full time'):
+        full_hours = 8
+    elif(check_attendance() =='absent'):
+        full_hours =0
+    Wage_per_hour =20
+    return Wage_per_hour*full_hours
+
+
 def main():
     print("The employee is ",check_attendance())
-
+    print("daily wage",daily_wage())
 
 if __name__ =='__main__':
      main()
