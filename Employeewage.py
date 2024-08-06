393dad9cf56fac77789f92e40c088b8531fd6236
sanjaysanju673 <sanjuvadde1111@gmail.com>
2024-08-06 12:01:23 +0530

    [Sanjay] Add:program to compute the wage for multiple companys

--- Employeewage.py
+++ Employeewage.py
@@ -1,8 +1,8 @@
 '''
     @Author: v sanjay kumar
-    @Date: 2024-07-03 02:00:30
+    @Date: 2024-07-04 11:00:30
     @Last Modified by: v sanjay kumar
-    @Last Modified time: 2024-07-03 02:00:30
+    @Last Modified time: 2024-07-04 11:00:30
     @Title : Employee wage problem
 '''
 
@@ -11,14 +11,9 @@ import random
 class Employee:
     def __init__(self, name):
         self.name = name
-        self.wage_per_hour = 20
-        self.full_time_hours = 8
-        self.part_time_hours = 4
-        self.total_hours = 0
-        self.total_days = 0
-        self.monthly_wage = 0
 
-    def check_attendance(self):
+    @staticmethod
+    def check_attendance():
         """
         Check the attendance for an employee.
 
@@ -34,6 +29,42 @@ class Employee:
             return 'part time'
         else:
             return 'absent'
+
+    def calculate_monthly_wage(self, company):
+        """
+        Calculate the total monthly wage for an employee for a given company.
+
+        Parameters:
+        - company: Company object containing wage parameters
+
+        Returns:
+        - Total monthly wage of the employee
+        """
+        total_hours = 0
+        total_days = 0
+        total_wage = 0
+        monthly_wages=[]
+        while total_hours <= company.max_working_hours and total_days < company.max_working_days:
+            attendance = self.check_attendance()
+            daily_wage = company.calculate_daily_wage(attendance)
+            monthly_wages.append(daily_wage)
+            total_wage += daily_wage
+            if attendance == 'full time':
+                total_hours += company.full_time_hours
+            elif attendance == 'part time':
+                total_hours += company.part_time_hours
+            total_days += 1
+        print("Employee monthly wages list",monthly_wages)
+        return total_wage
+
+class Company:
+    def __init__(self, name, wage_per_hour, max_working_days, max_working_hours):
+        self.name = name
+        self.wage_per_hour = wage_per_hour
+        self.full_time_hours = 8
+        self.part_time_hours = 4
+        self.max_working_days = max_working_days
+        self.max_working_hours = max_working_hours
 
     def calculate_daily_wage(self, attendance):
         """
@@ -52,79 +83,39 @@ class Employee:
         else:
             hours = 0
         wage = self.wage_per_hour * hours
-        print(f'{self.name} wage is {wage}')
         return wage
-
-    def calculate_monthly_wage(self):
-        """
-        Calculate the total monthly wage for an employee.
-        """
-        for _ in range(20):
-            attendance = self.check_attendance()
-            daily_wage = self.calculate_daily_wage(attendance)
-            self.monthly_wage += daily_wage
-        print(f"{self.name} employee monthly wage is {self.monthly_wage}")
-        return self.monthly_wage
-
-    def wage_per_condition(self):
-        """
-        Calculate the total monthly wage for an employee based on conditions.
-        """
-        while self.total_hours <= 100 and self.total_days <= 20:
-            attendance = self.check_attendance()
-            if attendance == 'full time':
-                self.total_hours += self.full_time_hours
-                wage = self.calculate_daily_wage(attendance)
-            elif attendance == 'part time':
-                self.total_hours += self.part_time_hours
-                wage = self.calculate_daily_wage(attendance)
-            else:
-                wage = 0
-            self.monthly_wage += wage
-            self.total_days += 1
-        print('Employee monthly wage', self.monthly_wage)
-        return self.monthly_wage
-
 
 def main():
     """
     Display the menu and handle user choices.
     """
+    companies = {
+        "tcs": Company("tcs", 20, 20, 100),
+        "apple": Company("apple", 25, 22, 120),
+        "techmahindra": Company("techmahindra", 22, 18, 90),
+    }
+
     while True:
         try:
             print("Menu:")
-            print("1. Check Attendance")
-            print("2. Calculate Full-Time Daily Wage")
-            print("3. Calculate Part-Time Daily Wage")
-            print("4. Calculate Monthly Wage")
-            print("5. Calculate Wage Per Condition")
-            print("6. Exit")
+            print("1. Calculate Monthly Wage for Company")
+            print("2. Exit")
             choice = int(input("Enter Your Choice: "))
 
             if choice == 1:
-                employee = Employee(input("Enter the name of the employee: "))
-                attendance = employee.check_attendance()
-                print(f"Attendance: {attendance}")
+                name = input("Enter the name of the employee: ")
+                employee = Employee(name)
+                company_name = input("Enter the company name (tcs, techmahindra, apple): ")
+
+                if company_name in companies.keys():
+                    company = companies[company_name]
+                    total_wage = employee.calculate_monthly_wage(company)
+                    print(employee.calculate_monthly_wage(company))
+                    print(f"{name} employee monthly wage for {company_name} is {total_wage}")
+                else:
+                    print("Invalid company name. Please enter a valid company.")
 
             elif choice == 2:
-                employee = Employee(input("Enter the name of the employee: "))
-                attendance = employee.check_attendance()
-                employee.calculate_daily_wage(attendance)
-
-            elif choice == 3:
-                employee = Employee(input("Enter the name of the employee: "))
-                attendance = employee.check_attendance()
-                employee.calculate_daily_wage(attendance)
-
-            elif choice == 4:
-                employee = Employee(input("Enter the name of the employee: "))
-                employee.calculate_monthly_wage()
-
-            elif choice == 5:
-                employee = Employee(input("Enter the name of the employee: "))
-                employee.wage_per_condition()
-
-            elif choice == 6:
                 print("Exiting the program.")
                 break
 
